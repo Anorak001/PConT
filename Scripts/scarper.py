c40ab464890534017693b599dc3340662917db79
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-09-16 18:57:59 +0530

    Update scarper.py

--- Scripts/scarper.py
+++ Scripts/scarper.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 # Define the base URL for the API
 base_url = "https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=protocolipport&format=text"
@@ -7,15 +6,11 @@ base_url = "https://api.proxyscrape.com/v3/free-proxy-list/get?request=displaypr
 # Make the request to the API and get the response
 response = requests.get(base_url)
 
-# Counter to check for success of the resposnse
+# Counter to check for success of the response
 if response.status_code == 200:
-    # Parse the JSON response
-    json_response = json.loads(response.text)
-
-    # write all the proxies that have been acquired using post into a file named "free_proxies.txt"
+    # Write all the proxies that have been acquired into a file named "free_proxies.txt"
     with open('free_proxies.txt', 'w') as f:
-        for item in json_response:
-            f.write(str(item) + "\n")
+        f.write(response.text)  # Write the raw response text directly into the file
 
 else:
     print("Error: Unable to access the API. Response status code:", response.status_code)
